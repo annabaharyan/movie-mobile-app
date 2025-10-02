c2af13dfd6191c7fa7ca34ef6c0695450db5380e
Anna Baharyan <anna.baharyan@gohacktech.com>
2025-10-02 15:42:06 +0400

    movie data ui, search page

--- app/movies/[id].tsx
+++ app/movies/[id].tsx
@@ -1,12 +1,16 @@
-import {View, Text} from "react-native";
-import {useLocalSearchParams} from "expo-router";
+import { View, Text, Button } from "react-native";
+import { useLocalSearchParams, useRouter } from "expo-router";
 
-const MovieDetails=()=>{
-  const {id}=useLocalSearchParams()
+const MovieDetails = () => {
+  const { id } = useLocalSearchParams();
+  const router = useRouter();
   return (
     <View>
       <Text>Movie Details: {id}</Text>
+      <Text className="text-primary font-bold pt-20">
+        <Button title="Go Back" onPress={() => router.push("/")} />
+      </Text>
     </View>
-  )
-}
-export default MovieDetails
+  );
+};
+export default MovieDetails;
